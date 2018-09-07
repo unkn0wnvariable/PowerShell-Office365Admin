c6da27e083dce857bd29ca1fd4d324d00e7ccc91
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-09-07 11:29:58 +0100

    New script

---
+++ ExchangeWithMFA/AddRoomCalendarEditors.ps1
@@ -0,0 +1,39 @@
+# Create a group and add it to all room mailboxes as an editor
+#
+# Uses the new PowerShell "module" that support MFA.
+#
+
+# What do you want the editors group to be called?
+$editorsGroup = 'From79 Calendar Editors'
+
+# Who do you want to be in the group?
+$editorsMembers = @('oandrews','oandrewsadmin')
+
+# Find and load the new ExO "module"
+$exoModulePath = (Get-ChildItem -Path $env:userprofile -Filter CreateExoPSSession.ps1 -Recurse -Force -ErrorAction SilentlyContinue).DirectoryName[-1]
+. "$exoModulePath\CreateExoPSSession.ps1"
+
+# Establish a session to Exchange Online
+Connect-EXOPSSession
+
+# Get all room mailboxes in the organisation
+$roomMailboxes = (Get-Mailbox -RecipientTypeDetails RoomMailbox).Alias
+
+# If the editors group doesn't exist, create it.
+if (!(Get-DistributionGroup -Identity $editorsGroup -ErrorAction SilentlyContinue)) {
+    New-DistributionGroup -Name $editorsGroup -Type Security
+}
+
+# Add members to the group
+$existingMembers = Get-DistributionGroupMember -Identity $editorsGroup
+foreach ($editorsMember in $editorsMembers) {
+    if ($editorsMember -notin $existingMembers.Name) {
+        Add-DistributionGroupMember -Identity $editorsGroup -Member $editorsMember
+    }
+}
+    
+# Add the permissions to the mailboxes
+foreach ($roomMailbox in $roomMailboxes) {
+    $calendarFolder = $mailboxAlias + ':\calendar'
+    Add-MailboxFolderPermission -Identity $calendarFolder -User $editorsGroup  -AccessRights Editor
+}
