6128b74cc0bf9c67c0348285de9b1bdb138118f0
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-09-12 16:06:07 +0100

    New script.

---
+++ MSOnline/BulkAddAdditionalLicence.ps1
@@ -0,0 +1,22 @@
+# Bulk Add a new licence to users on the basis of what licence they currently have
+#
+# This is useful for something like adding Office 365 ATP to everyone who currently has E3, for example.
+#
+
+# What licence do the users currently have?
+$existingLicence = ''
+
+# What licence are we adding?
+$licenceToAdd = ''
+
+# Import MSOnline module and connect
+Import-Module MSOnline
+Connect-MsolService
+
+# Find everyone who has the existing licence but not the new one
+$users = Get-MsolUser -All | Where-Object {($_.licenses).AccountSkuId -match $existingLicence -and !(($_.licenses).AccountSkuId -match $licenceToAdd)}
+
+# Add the new licence
+foreach ($user in $users) {
+    Set-MSOLUserLicense –user $user.UserPrincipalName –AddLicenses $licenceToAdd
+}
