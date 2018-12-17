116835f4cf7a45b9af8772c5a041a29929ee9620
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-12-17 13:54:13 +0000

    Update ChangeOnlineUserUPN.ps1
    Added comments

--- MSOnline/ChangeOnlineUserUPN.ps1
+++ MSOnline/ChangeOnlineUserUPN.ps1
@@ -1,11 +1,16 @@
 # Update Online User UPN - when AD sync doesn't do it.
 
+# Import the MSOL module and connect
 Import-Module MSOnline
 Connect-MsolService
 
+# Whose UPN are we changing?
 $oldUPN = Read-Host -Prompt 'Enter user''s old UPN in the format username@domain'
+
+# What are we changing it to?
 $newUPN = Read-Host -Prompt 'Enter user''s new UPN in the format username@domain'
 
+# Change the UPN
 if (Get-ADUser -Identity $newUPN.Split('@')[0]) {
     Set-MsolUserPrincipalName -UserPrincipalName $oldUPN -NewUserPrincipalName $newUPN
 }
