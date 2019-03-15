acd0ac32ce92b95dd9e4def1d69619f8f4319cf6
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2019-03-15 14:51:53 +0000

    Update ExportAllSkypeConferencing.ps1

--- SkypeForBusiness/ExportAllSkypeConferencing.ps1
+++ SkypeForBusiness/ExportAllSkypeConferencing.ps1
@@ -17,60 +17,52 @@ $acpInfoWildcard = '*BT*'
 # Output file
 $outputFile = 'C:\Temp\ConferencingUsers.csv'
 
-# Exclusion List
-$exclusionFile = 'C:\Temp\ExclusionList.txt'
-
-# Enable modern authentication
+# Get all conferencing users matching the above wildcarded info
 $conferencingUsers = Get-CsOnlineUser -WarningAction:SilentlyContinue -ErrorAction:SilentlyContinue | Where-Object {($_.Company -like $companyWildcard) -and ($_.AcpInfo -like $acpInfoWildcard) -and ($_.Enabled -eq $true)} | Select-Object DisplayName,Alias,AcpInfo
-
-# Get list of excluded users from file
-$excludedUsers = Get-Content -Path $exclusionFile
 
 # Set up user details hash table
 $userDetails = @()
 
 # Run through the users, check if they're in the exclusions list and if not then pull the details out of the AcpInfo code
 foreach ($conferencingUser in $conferencingUsers) {
-    if ($conferencingUser.Alias -notin $excludedUsers) {
-        $tollNumber = ''
-        if ([string]$conferencingUser.AcpInfo -match '<tollNumber>(?<tollNumber>.*)</tollNumber>') {
-            $tollNumber = $Matches.tollNumber
-        }
+    $tollNumber = ''
+    if ([string]$conferencingUser.AcpInfo -match '<tollNumber>(?<tollNumber>.*)</tollNumber>') {
+        $tollNumber = $Matches.tollNumber
+    }    
     
-        $tollFreeNumber = ''
-        if ([string]$conferencingUser.AcpInfo -match '<tollFreeNumber>(?<tollFreeNumber>.*)</tollFreeNumber>') {
-            $tollFreeNumber = $Matches.tollFreeNumber
-        }
+    $tollFreeNumber = ''
+    if ([string]$conferencingUser.AcpInfo -match '<tollFreeNumber>(?<tollFreeNumber>.*)</tollFreeNumber>') {
+        $tollFreeNumber = $Matches.tollFreeNumber
+    }
     
-        $participantPassCode = ''
-        if ([string]$conferencingUser.AcpInfo -match '<participantPassCode>(?<participantPassCode>.*)</participantPassCode>') {
-            $participantPassCode = $Matches.participantPassCode
-        }
+    $participantPassCode = ''
+    if ([string]$conferencingUser.AcpInfo -match '<participantPassCode>(?<participantPassCode>.*)</participantPassCode>') {
+        $participantPassCode = $Matches.participantPassCode
+    }
         
-        $domain = ''
-        if ([string]$conferencingUser.AcpInfo -match '<domain>(?<domain>.*)</domain>') {
-            $domain = $Matches.domain
-        }
+    $domain = ''
+    if ([string]$conferencingUser.AcpInfo -match '<domain>(?<domain>.*)</domain>') {
+        $domain = $Matches.domain
+    }
         
-        $name = ''
-        if ([string]$conferencingUser.AcpInfo -match '<name>(?<name>.*)</name>') {
-            $name = $Matches.name
-        }
+    $name = ''
+    if ([string]$conferencingUser.AcpInfo -match '<name>(?<name>.*)</name>') {
+        $name = $Matches.name
+    }
         
-        $url = ''
-        if ([string]$conferencingUser.AcpInfo -match '<url>(?<url>.*)</url>') {
-            $url = $Matches.url
-        }
+    $url = ''
+    if ([string]$conferencingUser.AcpInfo -match '<url>(?<url>.*)</url>') {
+        $url = $Matches.url
+    }
     
-        $userDetails += [PSCustomObject]@{
-            'DisplayName' = [string]$conferencingUser.DisplayName
-            'TollNumber' = [string]$tollNumber
-            'TollFreeNumber' = [string]$tollFreeNumber
-            'ParticipantPassCode' = [string]$participantPassCode
-            'Domain' = [string]$domain
-            'Name' = [string]$name
-            'Url' = [string]$url
-        }
+    $userDetails += [PSCustomObject]@{
+        'DisplayName' = [string]$conferencingUser.DisplayName
+        'TollNumber' = [string]$tollNumber
+        'TollFreeNumber' = [string]$tollFreeNumber
+        'ParticipantPassCode' = [string]$participantPassCode
+        'Domain' = [string]$domain
+        'Name' = [string]$name
+        'Url' = [string]$url
     }
 }
 
