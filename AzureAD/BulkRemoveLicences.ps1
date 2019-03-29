366b2d36cb448f26b629ed193196eb3859f0f7fa
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2019-03-29 09:03:53 +0000

    Update BulkRemoveLicences.ps1
    Removing licences with this module doesn't work in the way that Microsoft's documentation states.

--- AzureAD/BulkRemoveLicences.ps1
+++ AzureAD/BulkRemoveLicences.ps1
@@ -2,6 +2,9 @@
 #
 # List of available SKUs can be obtained with (Get-AzureADSubscribedSku).SkuPartNumber
 #
+# Removing licences with this module doesn't work in the way that Microsoft's documentation states.
+# I'm assuming this is a bug, so the code below works at time of creation but may stop working in the future.
+#
 
 # Where is the list of user UPN's?
 $userListPath = 'C:\Temp\UserList.txt'
@@ -16,24 +19,15 @@ Connect-AzureAD
 # Import list of users from file
 $userList = Get-Content -Path $userListPath | Sort-Object
 
-# Get user details from AzureAD
-$users = Get-AzureADUser -All $true | Where-Object {$_.UserPrincipalName -in $userList}
-
-# Get all available licence skus
-$skuIDs = (Get-AzureADSubscribedSku | Where-Object {$_.SkuPartNumber -in $licencesToRemove}).SkuId
-
 # Create a licenses object
 $licenses = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
 
-# Add the licences to remove to the licences object
-foreach ($skuID in $skuIDs) {
-    $license = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicense
-    $license.SkuId = $skuID
-    $licenses.RemoveLicenses += $license
-}
+# Get SkuIDs for licence(s) to be removed
+$licenses.RemoveLicenses = (Get-AzureADSubscribedSku | Where-Object {$_.SkuPartNumber -in $licencesToRemove}).SkuId
 
 # Remove the licenses
-foreach ($user in $users) {
+foreach ($username in $userList) {
+    $user = Get-AzureADUser -ObjectId $username
     if ($user.AccountEnabled -eq $true) {
         Set-AzureADUserLicense -ObjectId $user.UserPrincipalName -AssignedLicenses $licenses
         Write-Output -InputObject ('Licence remove from user account ' + $user.UserPrincipalName + '.')
