fc5883126f3b0386f28e1fb34a7b7b71fe97ca25
Oliver Andrews <3819627+unkn0wnvariable@users.noreply.github.com>
2022-02-04 14:21:19 +0000

    Update FindUsersWithMismatchedDomains.ps1
    Updated for current ExchangeOnlineManagement module

--- Exchange/FindUsersWithMismatchedDomains.ps1
+++ Exchange/FindUsersWithMismatchedDomains.ps1
@@ -2,19 +2,14 @@
 #
 
 # Establish a session to Exchange Online
-$credentials = Get-Credential -Message 'Enter your Exchange Online administrator credentials'
-$connectionParams = @{
-    'ConfigurationName' = 'Microsoft.Exchange';
-    'ConnectionUri' = 'https://outlook.office365.com/powershell-liveid/';
-    'Credential' = $credentials;
-    'Authentication' = 'Basic';
-    'AllowRedirection' = $true
-} 
-$exchangeSession = New-PSSession @connectionParams
-Import-PSSession -Session $exchangeSession
+Import-Module -Name ExchangeOnlineManagement
+Connect-ExchangeOnline
 
 # Get list of mailboxes with a different mail domain to UPN domain
-Get-Mailbox -ResultSize Unlimited | Where-Object {$_.UserPrincipalName.Split('@')[1] -ne $_.PrimarySmtpAddress.Split('@')[1]} | Format-Table Name,UserPrincipalName,PrimarySmtpAddress,RecipientTypeDetails
+$mismatchedMailboxes = Get-Mailbox -ResultSize Unlimited | Where-Object { $_.UserPrincipalName.Split('@')[1] -ne $_.PrimarySmtpAddress.Split('@')[1] }
+
+# Output as table
+$mismatchedMailboxes | Format-Table -Property Name, UserPrincipalName, PrimarySmtpAddress, RecipientTypeDetails -AutoSize
 
 # Disconnect from Exchange Online
-Remove-PSSession $exchangeSession
+Disconnect-ExchangeOnline
